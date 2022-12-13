099aaef9b1bd4e6751a627584d85679dc350628e
rbarongr <rafa.baron96@gmail.com>
2022-12-13 18:24:00 +0100

    minor update to Godot 4 Beta 8
    - player.gd syntax is following more closely the official GDScript style guide
    
    - New project icon
    
    - Updated the readme screenshot

--- Player/player.gd
+++ Player/player.gd
@@ -1,23 +1,23 @@
 extends CharacterBody3D
 
-var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 @export_range(1, 35, 1) var speed : float = 10 # m/s
-@export_range(10, 400, 1) var acceleration : float = 100
-@export_range(10, 400, 1) var deceleration : float = 100
+@export_range(10, 400, 1) var acceleration : float = 100 # m/s^2
+@export_range(10, 400, 1) var deceleration : float = 100 # m/s^2
 
 @export_range(0.1, 3.0, 0.1) var jump_height : float = 1 # m
 @export_range(0.1, 3.0, 0.1) var mouse_sens : float = 1
 
-@onready var camera : Camera3D = $Camera
-
-var input_dir : Vector2
-
-var walk_vel : Vector3
-var grav_vel : Vector3
-var jump_vel : Vector3
-
 var jumping : bool
+
+var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var input_dir : Vector2 # Player input
+
+var walk_vel : Vector3 # Walking velocity 
+var grav_vel : Vector3 # Gravity velocity 
+var jump_vel : Vector3 # Jumping velocity
+
+@onready var camera : Camera3D = $Camera
 
 func _ready() -> void:
 	capture_mouse(true)
@@ -28,21 +28,27 @@ func _input(event : InputEvent) -> void:
 	if Input.is_action_just_pressed("jump"): jumping = true
 	if Input.is_action_just_pressed("exit"): get_tree().quit()
 
+func _physics_process(delta : float) -> void:
+	velocity = _walk(delta) + _gravity(delta) + _jump(delta)
+	move_and_slide()
+
+func capture_mouse(capture : bool) -> void:
+	if capture:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func _aim(event : InputEvent) -> void:
 	var mouse_axis : Vector2 = event.relative if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED else Vector2.ZERO
 	camera.rotation.y -= mouse_axis.x * mouse_sens * .001
 	camera.rotation.x = clamp(camera.rotation.x - mouse_axis.y * mouse_sens * .001, -1.5, 1.5)
 
-func _physics_process(delta : float) -> void:
-	velocity = _walk(delta) + _gravity(delta) + _jump(delta)
-	@warning_ignore(return_value_discarded)
-	move_and_slide()
-
 func _walk(delta : float) -> Vector3:
 	if input_dir:
 		var camera_dir : Vector3 = camera.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)
 		walk_vel = walk_vel.move_toward(Vector3(camera_dir.x, 0, camera_dir.z).normalized() * speed, acceleration * delta)
-	else: walk_vel = walk_vel.move_toward(Vector3.ZERO, deceleration * delta)
+	else:
+		walk_vel = walk_vel.move_toward(Vector3.ZERO, deceleration * delta)
 	return walk_vel
 
 func _gravity(delta : float) -> Vector3:
@@ -52,9 +58,6 @@ func _gravity(delta : float) -> Vector3:
 func _jump(delta : float) -> Vector3:
 	if jumping:
 		jumping = false; if is_on_floor(): jump_vel = Vector3(0, sqrt(4 * jump_height * gravity), 0)
-	else: jump_vel = Vector3.ZERO if is_on_floor() else jump_vel.move_toward(Vector3.ZERO, gravity * delta)
+	else:
+		jump_vel = Vector3.ZERO if is_on_floor() else jump_vel.move_toward(Vector3.ZERO, gravity * delta)
 	return jump_vel
-
-func capture_mouse(capture : bool) -> void:
-	if capture: Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
-	else: Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Godot 4 - Basic First Person Controller
-This project contains the basic implementation of a First Person character controller for the [Godot 4 [Beta 5]](https://downloads.tuxfamily.org/godotengine/4.0/beta5/).
+This project contains the basic implementation of a First Person character controller for the [Godot 4 [Beta 8]](https://downloads.tuxfamily.org/godotengine/4.0/beta8/).
 
 The code found in [player.gd](Player/player.gd) uses vector forces to move the player. You can tweak the values of the forces in the editor directly. The _velocity_ of the player is calculated as a result of the vector sum of different vector forces: walk, jump and gravity.
 
 The main scene is a _Sandbox_ scene used to test the controls:
 
-![EditorView](Assets/Godot4Beta3FPC.png)
+![EditorView](Assets/BasicFPCBeta8.png)
 
 ## Controls
 | Keys | Action Name | Description |
