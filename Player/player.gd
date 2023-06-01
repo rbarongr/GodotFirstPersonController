821e151081194781d1c35c494e79066f8d02f554
nano13 <nano13@gmx.net>
2023-06-01 09:12:59 +0200

    implemented shift-walk with half the speed

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,15 @@
 class_name Player extends CharacterBody3D
 
 @export_category("Player")
-@export_range(1, 35, 1) var speed: float = 10 # m/s
-@export_range(10, 400, 1) var acceleration: float = 100 # m/s^2
+@export_range(1, 35, 1) var speed_run: float = 10 # m/s
+@export_range(1, 35, 1) var speed_walk: float = speed_run / 2 # m/s
+@export_range(10, 400, 1) var acceleration: float = 10000 # m/s^2
 
-@export_range(0.1, 3.0, 0.1) var jump_height: float = 1 # m
-@export_range(0.1, 9.25, 0.05, "or_greater") var camera_sens: float = 3
+@export_range(0.1, 3.0, 0.1) var jump_height: float = 1.5 # m
+@export_range(0.1, 9.25, 0.05, "or_greater") var camera_sens: float = 4
 
 var jumping: bool = false
+var speed_walking: bool = false
 var mouse_captured: bool = false
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -26,6 +28,8 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion: look_dir = event.relative * 0.01
+	if Input.is_action_pressed("move_walk"): speed_walking = true
+	if Input.is_action_just_released("move_walk"): speed_walking = false
 	if Input.is_action_just_pressed("jump"): jumping = true
 	if Input.is_action_just_pressed("exit"): get_tree().quit()
 
@@ -52,6 +56,9 @@ func _walk(delta: float) -> Vector3:
 	move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
 	var _forward: Vector3 = camera.transform.basis * Vector3(move_dir.x, 0, move_dir.y)
 	var walk_dir: Vector3 = Vector3(_forward.x, 0, _forward.z).normalized()
+	
+	var speed = speed_run
+	if speed_walking: speed = speed_walk
 	walk_vel = walk_vel.move_toward(walk_dir * speed * move_dir.length(), acceleration * delta)
 	return walk_vel
 
