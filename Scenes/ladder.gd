9da48d286aeb12fa0fbd5f1ce51f86a13181088f
nano13 <nano13@gmx.net>
2023-06-07 23:59:53 +0200

    renamed state variables to be prefixed with "state_"

--- Scenes/ladder.gd
+++ Scenes/ladder.gd
@@ -13,17 +13,17 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.name == "Player":
 		body.ladder_array.append(self)
-		if body.movement_state_current == body.MovementStates.LAND:
-			body.movement_state_current = body.MovementStates.LADDER_LAND_ATTACHED
-		elif body.movement_state_current == body.MovementStates.SWIM:
-			body.movement_state_current = body.MovementStates.LADDER_WATER_ATTACHED
+		if body.state_movement_current == body.MovementStates.LAND:
+			body.state_movement_current = body.MovementStates.LADDER_LAND_ATTACHED
+		elif body.state_movement_current == body.MovementStates.SWIM:
+			body.state_movement_current = body.MovementStates.LADDER_WATER_ATTACHED
 
 func _on_body_exited(body):
 	if body.name == "Player":
 		body.ladder_array.erase(self)
 		if body.ladder_array.size() == 0:
-			if body.movement_state_current == body.MovementStates.LADDER_LAND:
-				body.movement_state_current = body.MovementStates.LAND
-			elif body.movement_state_current == body.MovementStates.LADDER_WATER:
-				body.movement_state_current = body.MovementStates.SWIM
+			if body.state_movement_current == body.MovementStates.LADDER_LAND:
+				body.state_movement_current = body.MovementStates.LAND
+			elif body.state_movement_current == body.MovementStates.LADDER_WATER:
+				body.state_movement_current = body.MovementStates.SWIM
 
